f2cbd501d147cfbe470ffe4851ca31951659dfdd
Undisclosed <undisclosed674@gmail.com>
2021-12-27 02:30:09 -0800

    add window onLoad function

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //select navbar's toggle button and links
 const toggleButton = document.querySelector('.toggle-button');
 const navbarLinks = document.querySelector('.nav-links');
-//add click event listener to toggleButton and toggle the active class in navLinks
+//add click event listener to toggleButton and toggle the active class in navbarLinks
 toggleButton.addEventListener("click",() => {
 navbarLinks.classList.toggle('active');
 })
