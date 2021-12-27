c68294ebba93757578b28c4d0a10d1ca3e532f76
Undisclosed <undisclosed674@gmail.com>
2021-12-26 21:34:57 -0800

    replace dummy text and links with actual ones

--- main.js
+++ main.js
@@ -6,3 +6,20 @@ toggleButton.addEventListener("click",() => {
 navbarLinks.classList.toggle('active');
 })
 
+//Get the button
+var buttonUp = document.getElementById("btnToTop");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    buttonUp.style.display = "block";
+  } else {
+    buttonUp.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
